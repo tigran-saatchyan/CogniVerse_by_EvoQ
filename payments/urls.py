1bbbbb90289622a265d61ae5e6bdea2c542f44d7
Tigran Saatchyan <mr.saatchyan@gmail.com>
2023-09-19 16:36:12 +0400

    [FEATURE] Adds payment model and fixture to fill sample data

--- payments/urls.py
+++ payments/urls.py
@@ -14,17 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from rest_framework.routers import DefaultRouter
+
+from lessons.apps import LessonsConfig
+from lessons.views import (
+    LessonCreateView, LessonListView, LessonRetrieveView, LessonUpdateView,
+    LessonDestroyView
+)
+
+app_name = LessonsConfig.name
+
+router = DefaultRouter()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
-    path('courses/', include('courses.urls', namespace='courses')),
-    path('lessons/', include('lessons.urls', namespace='lessons')),
-    path('payments/', include('payments.urls', namespace='payments')),
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('create/', LessonCreateView.as_view(), name='lesson-create'),
+    path('', LessonListView.as_view(), name='lesson-list'),
+    path('<int:pk>', LessonRetrieveView.as_view(), name='lesson-retrieve'),
+    path('update/<int:pk>', LessonUpdateView.as_view(), name='lesson-update'),
+    path('destroy/<int:pk>', LessonDestroyView.as_view(), name='lesson-destroy'),
+] + router.urls
