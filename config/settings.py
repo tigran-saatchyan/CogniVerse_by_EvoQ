5b15c620ed34dc38c1ca1ff089109e0674edc7e3
Tigran Saatchyan <mr.saatchyan@gmail.com>
2023-09-25 12:45:53 +0400

    [FEATURE] implemented subscription for course updstes notification to be implemented

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,7 @@ LOCAL_APPS = [
     'courses.apps.CoursesConfig',
     'lessons.apps.LessonsConfig',
     'payments.apps.PaymentsConfig',
+    'subscribers.apps.SubscribersConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
